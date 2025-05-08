f23d860cd47730adfbce0161e4dc65a176b04852
Vitor Mignoni <vichsort@proton.me>
2025-05-07 22:25:54 -0300

    matrix comp, delete later

--- src/main.js
+++ src/main.js
@@ -7,27 +7,37 @@ import './components/navbar.js';
 export class MainElement extends LitElement {
   render() {
     return html`
-      <navbar-element></navbar-element>
-      <encript-form></encript-form>
-      <decript-form></decript-form>
+      <navbar>
+        <navbar-element></navbar-element>
+      </navbar>
+      
+      <main>
+        <encript-form></encript-form>
+        <decript-form></decript-form>
+      </main>
+
     `
   }
 
   static get styles() {
     return css`
       :host {
+        display: flex;
         max-width: 1280px;
         margin: 0 auto;
         padding: 2rem;
         text-align: center;
-        display: flex;
+        flex-direction: column;
         gap: 2rem;
         justify-content: center;
         align-items: flex-start;
       }
 
+      main {
+        display: flex;
+      }
+
       encript-form, decript-form {
-        flex: 1;
         border: 1px solid #ccc;
         padding: 1rem;
         box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
