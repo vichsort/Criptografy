4bd66e2017c480c0cbd0b97c053d1dd80d12d5ef
Vitor Mignoni <vichsort@proton.me>
2025-05-10 23:43:09 -0300

    kota start

---
+++ src/components/footer.js
@@ -0,0 +1,30 @@
+import { LitElement, css, html } from 'lit';
+
+class FooterElement extends LitElement {
+    render() {
+        return html`
+            <a href="#">Enctypt by Matrix</a>
+            <a href="#">Encypt by Cipher of Ceasar</a>
+            <a href="#">Encypt by Assimetric key</a>
+            <a href="#">Encypt by Hash</a>
+        `;
+    }
+    
+    static get styles() {
+        return css`
+            :host {
+                background-color: green;
+                padding: 1rem 6rem;
+                display: flex;
+                justify-content: center;
+            }
+
+            img {
+                width: 10rem;
+                height: 4rem;
+            }
+        `
+    }
+}
+
+window.customElements.define('footer-element', FooterElement)
